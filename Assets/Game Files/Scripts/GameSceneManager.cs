9591f72aacbb00397b3cbd7763624ee2dea4dfe7
Emil Z <damienzemanek@lcad.edu>
2026-05-30 18:07:27 -0700

    Menus

---
+++ Assets/Game Files/Scripts/GameSceneManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneManager : Singleton<GameSceneManager>
+{
+    public enum Scenes
+    {
+        Menu,
+        Level1,
+        Win
+    }
+    
+    
+    [Serializable]
+    public struct SceneData
+    {
+        public Scenes scene;
+        public int level;
+    }
+    
+    [SerializeField] public List<SceneData> sceneData;
+
+    public int GetScene(Scenes scene) => sceneData.FindIndex(s => s.scene == scene);
+}
---
+++ Assets/Game Files/Scripts/SceneAdapter.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneAdapter : MonoBehaviour
+{
+    public GameSceneManager.Scenes sceneToLoad;
+    public void LoadScene() => SceneManager.LoadScene(GameSceneManager.Instance.GetScene(sceneToLoad));
+    public void Quit() => Application.Quit();
+}
